74d8ff53273d8bc13626e3d51d2479a4fc225f1a
Amir Zarei <118467205+AmirAbaris@users.noreply.github.com>
2024-07-23 18:42:09 +0330

    [REFACTOR] cleaned up extra components

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,7 @@
-import React from 'react';
-import './App.css'
-
-const Counter = React.lazy(() => import('./components/Counter'));
-
 function App() {
   return (
     <div className='flex flex-col gap-y-12 items-center justify-center min-h-dvh'>
-      <Counter />
+
     </div>
   );
 }
